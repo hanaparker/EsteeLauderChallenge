4ba33cb5a2592d55b9ebab89d2ac2ca9a949b0a7
Hana Parker <hanaparker@icloud.com>
2023-08-05 21:38:16 -0500

    Initial Commit

---
+++ EsteeLauderChallenge/EsteeLauderChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderChallengeApp.swift
+//  EsteeLauderChallenge
+//
+//  Created by Hana Parker on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
